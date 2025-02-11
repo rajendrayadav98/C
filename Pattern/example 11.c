3b85ed1b5f25a4c9b83762488a226db96f788ca1
rajendrayadav98 <rajendra44556@gmail.com>
2025-02-11 22:01:56 +0530

    example 11
    example 11

---
+++ Pattern/example 11.c
@@ -0,0 +1,17 @@
+// Write the c programe of given pattern.
+// E E E E E
+// D D D D D
+// C C C C C
+// B B B B B
+// A A A A A 
+
+#include <stdio.h>
+    int main() {
+    for(int i='E';i>='A';i--){
+    for(int j='E';j>='A';j--){
+    printf("%2c",i);
+    }
+    printf("\n");
+    }
+    return 0;
+    }
